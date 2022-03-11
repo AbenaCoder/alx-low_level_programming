bdcabe674356f03b95e1867fce9e82c663812b16
AbenaCoder <AbenaCoder@gmail.com>
2022-03-10 18:36:55 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,4 +4,8 @@
  *
  * Return: Always O (Success)
  */
-int main(main){printf("with proper grammar, but the outcome is a piece of art,\n");
+int main(main)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
