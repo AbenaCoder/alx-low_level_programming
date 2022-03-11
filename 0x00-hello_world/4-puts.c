4462ab6ec6d214eb8f50c3a890cc5d1331c79682
AbenaCoder <AbenaCoder@gmail.com>
2022-03-10 18:30:09 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always O (Success)
  */
-int main(void){puts("\"Programming is like building a return (0);}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+			}
