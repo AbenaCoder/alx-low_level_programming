c73363973f4ce5ef0b0766c0ac1b285e2d0b077d
AbenaCoder <AbenaCoder@gmail.com>
2022-03-21 20:53:25 -0700

    0main

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* main - check the code 
+* main - check the code
 *
 * Return: Always 0.
 */
@@ -15,4 +15,4 @@ printf("n=%d\n", n);
 reset_to_98(&n);
 printf("n=%d\n", n);
 return (0);
-} 
+}
