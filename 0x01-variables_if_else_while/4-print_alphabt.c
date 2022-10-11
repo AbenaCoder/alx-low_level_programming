5d7bf9c0c4add63d96ee719d875bdfef2fa91843
root <eunicemaabenayayra@gmail.com>
2022-10-11 08:53:06 -0700

    numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+* Main - Print all alphabets except q and e
+* Return - Success(0)
+*/
+int main(void)
+{
+char c
+for (c = 'a' ; c = 'z');
+c++;
+{
+putchar(c != 'q' && c != 'e');
+
+putchar(c);
+
+putchar("\n");
+}
+
+return (0);
+}
