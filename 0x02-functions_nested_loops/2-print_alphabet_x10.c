d743f2a6aab5f21967294dc542fcb34dc6d664ff
root <eunicemaabenayayra@gmail.com>
2022-10-12 04:23:20 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,12 @@ void print_alphabet_x10(void)
 {
 	int letter;
 	int i;
+	i = 0
 
-	for (i = 0; i <= 9; i++)
+	if (i < 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
+		i++;
+		_purchar(letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar ('\n');
 	}
